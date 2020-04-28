bef8e3ce1174cb41b94e97197713e5ba672d396a
Gaetan Dupont <34641034+GatesDupont@users.noreply.github.com>
2020-04-28 16:09:59 -0400

    Update eame_ebirdst.R

--- post-source-code/eame_ebirdst.R
+++ post-source-code/eame_ebirdst.R
@@ -1,14 +1,19 @@
-install.packages("ebirdst")
+# Gates Dupont        #
+# For MassAudubon     #
+# dupont97@gmail.com  #
+# Feb. 4, 2020        #
+# # # # # # # # # # # # 
 
+# install.packages("ebirdst")
 library(ebirdst)
 library(raster)
 library(sf)
 library(rnaturalearth)
-library(leaflet)
 library(ggplot2)
 library(ggpubr)
 library(viridisLite)
 library(dplyr)
+library(leaflet.opacity)
 # handle namespace conflicts
 extract <- raster::extract
 
@@ -24,55 +29,42 @@ ma = us[match(toupper("Massachusetts"),toupper(us$NAME_1)),]
 
 "GETTING EBIRD DATA"
 
-# Download data (this takes time, ~20 mins for me)
+t0 = Sys.time()
+# Download data (this takes time (~20 mins for me), and space)
 sp_path <- ebirdst_download(species =  "easmea", tifs_only = FALSE)
-
+tf = Sys.time()
+t_total  = tf-t0
 
 "ABUNDANCE"
 
-# Load trimmed mean abundances
+# load trimmed mean abundances
 abunds <- load_raster("abundance_umean", path = sp_path)
 
-# Crop to an area rougly the size of MA
+# Cropping to an area rougly the size of MA
 # (Week 23 = June 6-12)
 abunds_23_cr =  crop(
   abunds[[23]], 
   extent(c(-6.2e6, -5.6e6,  4.5e6, 4.85e6)))
 
-# Define mollweide projection
+# define mollweide projection
 mollweide <- "+proj=moll +lon_0=-90 +x_0=0 +y_0=0 +ellps=WGS84"
 
-# Project single layer from stack to mollweide
+# project single layer from stack to mollweide
 week23_moll <- projectRaster(
   abunds_23_cr, 
-  crs = mollweide, method = "bilinear")
+  crs = mollweide, method = "ngb")
 
 # Mask to MA and crop
 ma_moll = spTransform(ma, mollweide)
 r = mask(week23_moll, ma_moll) %>%
   crop(., ma_moll) %>%
-  projectRaster(., crs = crs(ma), method = "bilinear")
-
-# Convert raster to data frame for ggplot
-r_spdf <- as(r, "SpatialPixelsDataFrame")
-r_df <- as.data.frame(r_spdf)
-colnames(r_df) <- c("value", "x", "y")
-
-
-"PLOT ABUNDANCE"
-ggplot() +
-  geom_raster(data = r_df , aes(x = x, y = y, fill = value)) + 
-  scale_fill_gradientn(colors = abundance_palette(10, season = "breeding")) +
-  coord_quickmap() +
-  theme_bw() +
-  theme(legend.position = "right") +
-  labs(title = "Eastern Meadowlark",
-       subtitle = "Relative Abundance, June 6-12",
-       caption = "Data source: eBird Status and Trends",
-       fill = "RA", y = "Latitude", x = "Longitude")
+  projectRaster(., crs = crs(ma), method = "ngb")
 
 
 "VARIABLE IMPORTANCE"
+
+# Load predictor importance
+pis = load_pis(sp_path)
 
 # Select region and season
 lp_extent <- ebirdst_extent(
@@ -83,20 +75,51 @@ lp_extent <- ebirdst_extent(
 par(mfrow = c(1, 1), mar = c(0, 0, 0, 6))
 calc_effective_extent(sp_path, ext = lp_extent)
 
-# Load predictor importance
-pis = load_pis(sp_path)
 
-# Plot
+"FINAL PLOT"
+
+# Convert raster to data frame for ggplot
+r_spdf <- as(r, "SpatialPixelsDataFrame")
+r_df <- as.data.frame(r_spdf)
+colnames(r_df) <- c("value", "x", "y")
+
+# RELATIVE ABUNDANCE
+ggplot() +
+  geom_raster(data = r_df , aes(x = x, y = y, fill = value)) + 
+  scale_fill_gradientn(colors = abundance_palette(10, season = "breeding")) +
+  coord_quickmap() +
+  theme_bw() +
+  # theme(legend.position = "right", # For exporting as png of 2000x1000
+  #       plot.title = element_text(size = 80),
+  #       plot.subtitle = element_text(size = 50),
+  #       plot.caption = element_text(size = 50),
+  #       axis.text = element_text(size = 40),
+  #       axis.title = element_text(size = 50),
+  #       legend.title = element_text(size = 50),
+  #       legend.text = element_text(size = 40)) +
+  # guides(fill = guide_colourbar(barwidth = 4, barheight = 40)) +
+  labs(title = "Eastern Meadowlark",
+       subtitle = "Relative Abundance, June 6-12",
+       caption = "Data source: eBird Status and Trends",
+       fill = "RA", y = "Latitude", x = "Longitude")
+
+# VARIABLE IMPORTANCE
 plot_pis(pis, ext = lp_extent, by_cover_class = TRUE, n_top_pred = 15)
+# I modified this function separately to produce stylistically similar plots
+
+
+library(leaflet)
 
 # Convert to leaflet CRS
 map_crs = sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 r4map = projectRaster(r, crs = map_crs, method = "bilinear")
 
-# Add some specifics
+# Add some options to the map
+basemaps = c("CartoDB.Positron", "OpenStreetMap")
 pal = colorNumeric(abundance_palette(10, season = "breeding"), values(r),
                     na.color = "transparent")
 map_attr = "© <a href='https://www.esri.com/en-us/home'>ESRI</a> © <a href='https://www.google.com/maps/'>Google</a> © <a href='https://ebird.org/science/status-and-trends'>eBird / Cornell Lab of Ornithology</a> © <a href='https://www.gatesdupont.com/'>Gates Dupont</a>"
+
 
 # Map
 eame_ma_lf <-leaflet() %>% 
@@ -106,7 +129,9 @@ eame_ma_lf <-leaflet() %>%
   addProviderTiles("OpenStreetMap", group = "Open Street Map") %>%
   addProviderTiles('Esri.WorldImagery', group = "ESRI") %>%
   addTiles(urlTemplate = "", attribution = map_attr) %>%
-  addRasterImage(r, colors = pal, opacity = 0.5, group = "Eastern Meadowlark")  %>%
+  addRasterImage(r, colors = pal, opacity = 0.5, layerId = "raster",
+                 group = "Eastern Meadowlark") %>%
+  addOpacitySlider(layerId = "raster")%>%
   addLegend(pal = pal, values = values(r),
             title = "Relative abundance") %>%
   leafem::addMouseCoordinates()  %>%
@@ -116,9 +141,9 @@ eame_ma_lf <-leaflet() %>%
     options = layersControlOptions(collapsed = FALSE)
   )
 
-# View map
 eame_ma_lf
 
+
 htmlwidgets::saveWidget(eame_ma_lf, 
-                        file = "eame_ma.html", 
+                        file = "~/Desktop/eame_ma.html", 
                         selfcontained = TRUE)
